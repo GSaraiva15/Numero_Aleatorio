97fa75345ccd08eb6e64333724b6b86d777af4b6
Gonçalo Saraiva <goncalosaraiva00@gmail.com>
2020-04-18 21:26:57 +0100

    projeto concluído

--- app/src/main/java/com/example/numero_aleatorio/MainActivity.java
+++ app/src/main/java/com/example/numero_aleatorio/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.numero_aleatorio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -15,33 +17,52 @@ public class MainActivity extends AppCompatActivity {
     private Random random = new Random();
     private int numeroAdivinhar = random.nextInt(10) + 1;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void verificaNumero(View view){
+    int soma;
+
+    public void verificaNumero(View view) {
 
 
-        EditText editTextNumero =(EditText)findViewById(R.id.editTextNumero);
+        EditText editTextNumero = (EditText) findViewById(R.id.editTextNumero);
 
         String strNumero = editTextNumero.getText().toString();
 
+
         int numero;
-        try{
-            numero =Integer.parseInt(strNumero);
-        }catch(NumberFormatException e) {
+        try {
+            numero = Integer.parseInt(strNumero);
+        } catch (NumberFormatException e) {
             editTextNumero.setError(getString(R.string.numero_obrigatorio));
             editTextNumero.requestFocus();
             return;
         }
-        if((numero < 1 )|| (numero > 10)){
+        if ((numero < 1) || (numero > 10)) {
             editTextNumero.setError(getString(R.string.numero_limites));
             editTextNumero.requestFocus();
             return;
         }
 
-    }
+        TextView textViewMostrarResultado = findViewById(R.id.textViewMostrarResultado);
+        TextView textViewMostrarTentativas = findViewById(R.id.textViewMostrarTentativas);
+
+
+        if (numeroAdivinhar == numero) {
+            soma++;
+            textViewMostrarResultado.setText(R.string.resultado_correto);
+            textViewMostrarResultado.setTextColor(Color.GREEN);
+            textViewMostrarResultado.requestFocus();
+        } else {
+            soma++;
+            textViewMostrarTentativas.setText(String.valueOf(soma));
+            textViewMostrarResultado.setText(R.string.resultado_errado);
+            textViewMostrarResultado.setTextColor(Color.RED);
+
+        }
 
     }
-
+}
